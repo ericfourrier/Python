0d97a2c0b37de4ced2461d66379acebd134f77d3
Eric Fourrier <ericfourrier0@gmail.com>
2015-05-17 11:13:53 -0700

    Fix issue, import methods when importing package

--- Quandl/__init__.py
+++ Quandl/__init__.py
@@ -9,26 +9,28 @@ __email__ = 'connect@quandl.com'
 __url__ = 'http://www.quandl.com/'
 __license__ = 'MIT'
 __version__ = '2.8.6'
-
 #############################################
 #
 # Don't import anything in __init__.py!
 # Otherwise setup.py can't import the private constants above (__authors__, etc)
 # because it won't have installed dependencies yet!
 #
-# from .Quandl import (
-#     get,
-#     push,
-#     search,
-#     WrongFormat,
-#     MultisetLimit,
-#     ParsingError,
-#     CallLimitExceeded,
-#     DatasetNotFound,
-#     ErrorDownloading,
-#     MissingToken,
-#     DateNotRecognized,
-#     CodeFormatError
-#     )
-#
+
+# Perso modif
+from .Quandl import (
+    get,
+    get_meta,
+    push,
+    search,
+    WrongFormat,
+    MultisetLimit,
+    ParsingError,
+    CallLimitExceeded,
+    DatasetNotFound,
+    ErrorDownloading,
+    MissingToken,
+    DateNotRecognized,
+    CodeFormatError
+    )
+
 ###############################################
